36fc33462b1ef7ba609ce6796f8cd2a24c477897
Janar Palk <janar.palk@gmail.com>
2016-11-30 14:36:20 +0200

    Migrated to vero js api

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,22 @@ $(document).ready(function() {
 		localStorage.removeItem("newsletter-feedback");
 	}
 	$('.newsletter-form form.sign-up').submit(function(event) {
+		event.preventDefault();
+		var email = $('.sign-up-input', $(this)).val();
+		_veroq.push(['user', {
+		  id: email,
+		  email: email,
+		  source: 'blog'
+		}]);
+		_veroq.push(['tags', {    
+		  id: email,    
+		  add: ['Blog subscription'],    
+		  remove: []  
+		}]);
 		localStorage.setItem("newsletter-subscribed", true);
 		localStorage.setItem("newsletter-feedback", true);
+
+		$('.newsletter-form .subscribe-inputs').hide();
+		$('.newsletter-form .subscribe-success').show();
 	});
 });
